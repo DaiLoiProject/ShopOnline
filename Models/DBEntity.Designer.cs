8b5c4802ca9561abe4f8631b4ab5ea85e1a51ae5
DaiLoiProject <dangdailoi732006@gmail.com>
2025-11-25 23:48:03 +0700

    fixdata1

--- Models/DBEntity.Designer.cs
+++ Models/DBEntity.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Downloads\FinalCode1\ShopOnlineDone\ShopOnlineDone\ShopOnlineDone\ShopOnline\Models\DBEntity.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\dangd\OneDrive\Tài liệu\FinalCode1\ShopOnlineDone\ShopOnlineDone\ShopOnlineDone\ShopOnline\Models\DBEntity.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
